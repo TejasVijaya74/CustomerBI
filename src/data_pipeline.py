816b25243bcb513a7e79cdc6c52856c8290c1557
TejasVijaya74 <vt6267700@gmail.com>
2024-11-24 19:20:49 +0530

    Path corrected

--- src/data_pipeline.py
+++ src/data_pipeline.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 # Load dataset
-data = pd.read_csv('data/customer_churn.csv')
+data = pd.read_csv('CustomerBI\data\customer_churn.csv')
 
 # Data preprocessing
 data.fillna(0, inplace=True)  # Fill missing values
 
 # Save preprocessed data
-data.to_csv('data/cleaned_customer_churn.csv', index=False)
-print("Data cleaned and saved to 'data/cleaned_customer_churn.csv'!")
+data.to_csv('CustomerBI\data\cleaned_customer_churn.csv', index=False)
+print("Data cleaned and saved to 'CustomerBI\data\cleaned_customer_churn.csv'!")
